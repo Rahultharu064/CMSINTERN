134b1f156db97fc18b7115a7a099229a4f5a2d59
Rahultharu064 <rahultharu46882@icloud.com>
2026-07-20 19:57:01 +0545

    first commit

---
+++ frontend/src/pages/Home.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Navbar from '../components/common/Navbar'
+import HeroSection from '../components/HomePageComponent/HeroSection'
+
+const Home = () => {
+  return (
+    <>
+    <Navbar />
+    <HeroSection />
+      
+    </>
+  )
+}
+
+export default Home
